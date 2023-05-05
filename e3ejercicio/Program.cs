b1a1ae4a401449aec676207a2d36bfdbbd3e32f6
Ezekielrd <132635482+Ezekielrd@users.noreply.github.com>
2023-05-04 23:40:16 -0600

    Add files via upload

---
+++ e3ejercicio/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e3ejercicio
+{
+    class Program
+    {
+        static void pedir(string m, out int n)
+        {
+            Console.WriteLine(m);
+            n = int.Parse(Console.ReadLine());
+        }
+        static void menu()
+        {
+            Console.WriteLine("\n\n\tMENU\n");
+            Console.WriteLine("1.encender la computadora");
+            Console.WriteLine("2.anadir un dato");
+            Console.WriteLine("3.eliminar un dato");
+            Console.WriteLine("4.apagart la computadora");
+            Console.WriteLine("5.salir");
+        }
+        static void ejecutar(computadora cp)
+        {
+            int opc;
+            do
+            {
+                int dato;
+                menu();
+                pedir("de la opcion: ", out opc);
+                Console.Clear();
+                switch (opc)
+                {
+                    case 1:
+                        cp.enceder(); break;
+                    case 2:
+                        pedir("de el tamaño del dato: ", out dato);
+                        cp.añadirDato(dato);
+                        break;
+                    case 3:
+                        pedir("de el tamaño del dato a eliminar: ", out dato);
+                        cp.eliminarDato(dato);
+                        break;
+                    case 4:
+                        cp.apagado();
+                        break;
+                    case 5:
+                        Console.WriteLine("saliendo...");
+                        break;
+                    default:
+                        Console.WriteLine("opcion fuera del rango");
+                        break;
+                }
+            }
+            while (opc != 5);
+        }
+        static void Main(string[] args)
+        {
+            computadora cp = new computadora(0, 200, false);
+            ejecutar(cp);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ e3ejercicio/computadora.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e3ejercicio
+{
+    class computadora
+    {
+        int capacidadDisco;
+        int espacioDispinible;
+        bool on;
+
+        public computadora(int cpacidadDisco, int espacioDispinible, bool on)
+        {
+            this.CapacidadDisco = cpacidadDisco;
+            this.EspacioDispinible = espacioDispinible;
+            this.On = on;
+        }
+
+        public int CapacidadDisco { get => capacidadDisco; set => capacidadDisco = value; }
+        public int EspacioDispinible { get => espacioDispinible; set => espacioDispinible = value; }
+        public bool On { get => on; set => on = value; }
+
+        public void añadirDato(int dato)
+        {
+            if (on)
+            {
+                if (dato < espacioDispinible)
+                {
+                    this.capacidadDisco += dato;
+                    Console.WriteLine("el dato se ha añadido");
+                }
+            }
+            else
+            {
+                Console.WriteLine("la computadora esta apagada");
+            }
+        }
+
+        public void eliminarDato(int dato)
+        {
+            if (on)
+            {
+                if (dato < capacidadDisco)
+                {
+                    capacidadDisco -= dato;
+                    Console.WriteLine("se ha elimida el dato");
+                }
+                else
+                {
+                    Console.WriteLine("el dato es mayor a lo almcanado en el disco");
+                }
+            }
+            else
+            {
+                Console.WriteLine("la computadora apagada");
+            }
+        }
+        public void enceder()
+        {
+            this.on = true;
+            Console.WriteLine("welcome");
+        }
+        public void apagado()
+        {
+            this.on = false;
+            Console.WriteLine("bye");
+        }
+    }
+}
