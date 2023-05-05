3d1e28b82eb00001eda2c00aae76f7166b19a74f
Ezekielrd <132635482+Ezekielrd@users.noreply.github.com>
2023-05-04 23:52:14 -0600

    Add files via upload

---
+++ 4ejercicio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            moto mt = new moto(01, "suize", "nas", 2000, 4500, 2013, true);
+            mt.Calprecio();
+            auto at = new auto(02,"toyota","nissan",5000,60000,2014,false);
+            at.Calprecio();
+            Console.ReadKey();
+        }
+    }
+}
